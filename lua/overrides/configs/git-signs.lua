cc22f8e4d7c2976f515dce7781cbf57af56e53ca
Stephanie Chau <schau@uc.cl>
2024-04-18 16:55:16 -0400

    chore(lua): move custom configs to "overrides" directory. Add override for gitsigns

---
+++ lua/overrides/configs/git-signs.lua
@@ -0,0 +1,16 @@
+return {
+  signs = {
+    add          = { text = "┃" },
+    change       = { text = "┃" },
+    delete       = { text = "󰍵" },
+    topdelete    = { text = "‾" },
+    changedelete = { text = "D" },
+    untracked    = { text = "┆" },
+  },
+
+  current_line_blame = true,
+  current_line_blame_formatter_opts = {
+    relative_time = true,
+  }
+}
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,22 +6,23 @@ return {
       require "configs.conform"
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require("configs.lspconfig")
+    end,
+  },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --  
   {
   	"williamboman/mason.nvim",
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
   			"html-lsp", "css-lsp" , "prettier",
+        "gopls", "goimports", "gofumpt",
+        "golangci-lint", "golangci-lint-langserver",
+        "gotests",
   		},
   	},
   },
@@ -31,7 +32,11 @@ return {
   	opts = {
   		ensure_installed = {
   			"vim", "lua", "vimdoc",
-       "html", "css"
+       "html", "css",
+        "markdown", "markdown_inline",
+        "bash",
+        "go", "gomod", "gosum", "gowork",
+        "json", "yaml",
   		},
   	},
   },
@@ -46,9 +51,22 @@ return {
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "nvimtree")
 
-      local result = require("configs.nvimtree")
+      local result = require("overrides.configs.nvimtree")
 
       require("nvim-tree").setup(vim.tbl_deep_extend("force", opts, result))
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require "nvchad.configs.gitsigns"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "git")
+      local result = require("overrides.configs.git-signs")
+      require("gitsigns").setup(vim.tbl_deep_extend("force", opts, result))
     end,
   },
 }
