5432e1b1ed3f5d1786e12c57202fa87b5442c666
Stephanie Chau <schau@uc.cl>
2024-04-18 19:12:09 -0400

    chore(*.go): add "Go" on steroids plugin for Neovim (w/ autofix and autofmt on save)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -69,5 +69,21 @@ return {
       require("gitsigns").setup(vim.tbl_deep_extend("force", opts, result))
     end,
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      -- "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+      require("overrides.configs.golang")
+    end,
+
+    event = {"CmdlineEnter"},
+    ft = {"go", "gomod"},
+    build = ":lua require('go.install').update_all_sync()" -- if you need to install/update all binaries
+  }
 }
 
